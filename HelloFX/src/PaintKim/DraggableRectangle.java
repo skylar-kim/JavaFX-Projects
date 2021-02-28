e596c504125cab64520102bc24767e92a0fb9e90
Skylar Kim <kimsooye@usc.edu>
2021-02-27 23:30:12 -0500

    hw4: rectangle is moving

--- HelloFX/src/PaintKim/DraggableRectangle.java
+++ HelloFX/src/PaintKim/DraggableRectangle.java
@@ -2,84 +2,81 @@ package PaintKim;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import mousing.Point;
 
-public class DraggableRectangle extends Rectangle{
+
+public class DraggableRectangle extends GeneralShape {
+	Rectangle r;
 	Point imAt;
 	private double x = 0;
 	private double y = 0;
 	private double mousex = 0;
 	private double mousey = 0;
-	
-	public DraggableRectangle(double xCoord, double yCoord) {
-		super(xCoord, yCoord);
-		
-		onMousePressedProperty().set(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent m) {
-				
-				// record the current mouse X and Y position
-				mousex = m.getSceneX();
-				mousey = m.getSceneY();
-				
-				x = getLayoutX();
-				y = getLayoutY();
-				
 
-			}
-		});
-		
-		onMouseDraggedProperty().set(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent m) {
-				double distx = m.getSceneX() - mousex;
-				double disty = m.getSceneY() - mousey;
-				
-				x += distx;
-				y += disty;
-				
-				setLayoutX(x);
-				setLayoutY(y);
-				
-				
-				mousex = m.getSceneX();
-				mousey = m.getSceneY();
-				
-				m.consume();
-			}
-		});
-		
-		onMouseClickedProperty().set(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent m) {
-				// not really do anything...
-			}
-		});
+	public DraggableRectangle(double xCoord, double yCoord) {
+		r = new Rectangle(xCoord, yCoord);
+		getChildren().add(r);
 	}
-	public void setimAt(double x, double y) {
-		imAt = new Point(x, y);
+
+	public Rectangle getRectangle() {
+		return this.r;
 	}
-	
-	public void dragged( Point p )
-    {
- 	   setLayoutX( p.getX() );
- 	   setLayoutY( p.getY() );
- 	   imAt = p;
- 	   System.out.println("imAt x: " + imAt.getX());
- 	   System.out.println("imAt Y: " + imAt.getY());
-    }
-	
-	public boolean zatYou( Point m )
-    {
-    	System.out.println("zatYou x="+m.getX()+" y="+m.getY());
-    	double d = Math.abs(m.getX()-imAt.getX())
-    			  +Math.abs(m.getY()-imAt.getY());
-    	System.out.println("d: " + d);
-    	boolean ret = (d<50);
-    	System.out.println("zatYou ret="+ret);
-    	return ret;
-    }
-	
-	
+
+
+	@Override
+	void setColor(Color c) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	@Override
+	protected boolean zatYou(PaintKim.Point p) {
+		System.out.println("zatYou x="+p.getX()+" y="+p.getY());
+		double x1 = this.r.getX();
+		double y1 = this.r.getY();
+		double x2 = x1 + this.r.getWidth();
+		//double y2 = y1;
+		//double x3 = x1;
+		double y3 = y1 + this.r.getHeight();
+		//double x4 = x2;
+		//double y4 = y3;
+		
+		boolean isOnRect = false;
+		isOnRect = ((p.getX() >= x1 && p.getX() <= x2) && (p.getY() >= y1 && p.getY() <= y3));
+		System.out.println("zatYou ret="+isOnRect);
+		return isOnRect;
+	}
+
+	@Override
+	void dragged(PaintKim.Point p, MouseEvent m) {
+		// TODO Auto-generated method stub
+		double distx = m.getSceneX() - this.mouseX;
+		double disty = m.getSceneY() - this.mouseY;
+		this.r.setLayoutX(distx);
+		this.r.setLayoutY(disty);
+		imAt = p;
+		System.out.println("imAt x: " + imAt.getX());
+		System.out.println("imAt Y: " + imAt.getY());
+	}
+
+	@Override
+	void setSelected(boolean bool) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void setMouseX(double x) {
+		// TODO Auto-generated method stub
+		this.mouseX = x;
+	}
+
+	@Override
+	void setMouseY(double y) {
+		// TODO Auto-generated method stub
+		this.mouseY = y;
+	}
+
 }
--- HelloFX/src/PaintKim/MainUI.java
+++ HelloFX/src/PaintKim/MainUI.java
@@ -16,7 +16,7 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
-import mousing.Point;
+
 
 public class MainUI extends Application {
 
@@ -31,6 +31,8 @@ public class MainUI extends Application {
 
 	// Line Stuff
 	Line l;
+	DraggableLine dl;
+	
 	// MyLine l;
 	boolean mouseIsPressedLine = false;
 	boolean drawLineBool = false;
@@ -43,10 +45,11 @@ public class MainUI extends Application {
 	DraggableRectangle theDR;
 	boolean mouseIsPressed = false;
 	
-	// List of items on screen
-	//List<Rectangle> rectangleList;
-	List<DraggableRectangle> drList;
+	DraggableLine theLine;
 	
+	
+	List<GeneralShape> itemList;
+	GeneralShape theItem;
 	
 	// MODES
 	enum Mode {RECT, LINE, SELECT, DELETE}
@@ -67,8 +70,7 @@ public class MainUI extends Application {
 		
 		//rectangleList = new ArrayList<Rectangle>();
 		
-		drList = new ArrayList<DraggableRectangle>();
-		
+		itemList = new ArrayList<GeneralShape>();
 		PaintMenu paintMenu = new PaintMenu();
 
 		root.getChildren().add(paintMenu);
@@ -90,11 +92,11 @@ public class MainUI extends Application {
 		*/
 	}
 	
-	public DraggableRectangle findDraggedRectangle(Point p) {
-		DraggableRectangle found = null;
-		for (DraggableRectangle dr: drList) {
-			if (dr.zatYou(p)) {
-				found = dr;
+	public GeneralShape findItem(Point p) {
+		GeneralShape found = null;
+		for (GeneralShape gs : itemList) {
+			if (gs.zatYou(p)) {
+				found = gs;
 			}
 		}
 		return found;
@@ -123,43 +125,28 @@ public class MainUI extends Application {
 
 			rectangleB.setOnAction(e -> {
 				System.out.println("Rectangle Button Pressed");
-//				drawLineBool = false;
-//
-//				drawRectangleBool = true;
-//				drawRectangle();
-				//dr = new DraggableRectangle();
-				//dr.drawRectangle();
-				//root.getChildren().add(dr);
-				//itemList.add(dr);
 				
 				currentMode = Mode.RECT;
 			});
 
 			lineB.setOnAction(e -> {
 				System.out.println("Line Button Pressed");
-//				drawRectangleBool = false;
-//
-//				drawLineBool = true;
-//				drawLine();
+
 				currentMode = Mode.LINE;
 			});
 
 			puttextB.setOnAction(e -> {
 				System.out.println("Put Text Button Pressed");
-//				drawLineBool = false;
-//				drawRectangleBool = false;
-//
-//				putTextBool = true;
-//				putText();
+
+				putText();
 			});
 			
 			selectB.setOnAction(e -> {
 				System.out.println("Select Button Pressed");
-//				drawLineBool = false;
-//				drawRectangleBool = false;
-//				
-//				select();
+
+				currentMode = Mode.SELECT;
 			});
+			
 			paintAction();
 
 			getChildren().addAll(rectangleB, lineB, selectB, deleteB, puttextB, textField, loadB, saveB);
@@ -170,14 +157,27 @@ public class MainUI extends Application {
 				if (currentMode == Mode.RECT) {
 					corners[0] = new Point(m);
 					dr = new DraggableRectangle(10, 10);
-					dr.setX(corners[0].getX());
-					dr.setY(corners[0].getY());
+					dr.getRectangle().setX(corners[0].getX());
+					dr.getRectangle().setY(corners[0].getY());
+					itemList.add(dr);
 					root.getChildren().add(dr);
-					mouseIsPressedRect = true;
+					mouseIsPressed = true;
 				}
 				else if (currentMode == Mode.LINE) {
-					l = new Line(m.getX(), m.getY(), m.getX(), m.getY());
-					root.getChildren().add(l);
+//					l = new Line(m.getX(), m.getY(), m.getX(), m.getY());
+//					root.getChildren().add(l);
+					//lineList.add(l);
+					dl = new DraggableLine(m.getX(), m.getY(), m.getX(), m.getY());
+					root.getChildren().add(dl);
+					itemList.add(dl);
+					mouseIsPressed = true;
+				}
+				else if (currentMode == Mode.SELECT) {
+					Point p = new Point(m);
+					theItem = findItem(p);
+					theItem.setMouseX(p.getX());
+					theItem.setMouseY(p.getY());
+					mouseIsPressed = true;
 				}
 			});
 			
@@ -186,227 +186,62 @@ public class MainUI extends Application {
 					corners[1] = new Point(m);
 					double xdif = corners[0].xdif(corners[1]);
 					double ydif = corners[0].ydif(corners[1]);
-					dr.setWidth(xdif);
-					dr.setHeight(ydif);
+					dr.getRectangle().setWidth(xdif);
+					dr.getRectangle().setHeight(ydif);
 					// put these lines back in to get tracking in all 4 quadrants
-					dr.setX(min(corners[0].getX(), corners[1].getX()));
-					dr.setY(min(corners[0].getY(), corners[1].getY()));
+					dr.getRectangle().setX(min(corners[0].getX(), corners[1].getX()));
+					dr.getRectangle().setY(min(corners[0].getY(), corners[1].getY()));
 				}
 				else if (currentMode == Mode.LINE) {
-					l.setEndX(m.getX());
-					l.setEndY(m.getY());
+//					l.setEndX(m.getX());
+//					l.setEndY(m.getY());
+					dl.getLine().setEndX(m.getX());
+					dl.getLine().setEndY(m.getY());
+				}
+				else if (currentMode == Mode.SELECT) {
+					Point p = new Point (m);
+					if (theItem != null) {
+						theItem.dragged(p, m);
+					}
 				}
 			});
-		}
-		/*
-		public void drawRectangle() {
-			if (drawRectangleBool) {
-				System.out.println("Drawing Rectangle");
-				System.out.println("drawRectangleBool: " + drawRectangleBool);
-
-				root.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent m) -> {
-//					corners[0] = new Point(e.getX(), e.getY());
-//					r = new Rectangle(10, 10);
-//					r.setX(corners[0].getX());
-//					r.setY(corners[0].getY());
-//					root.getChildren().add(r);
-					corners[0] = new Point(m);
-					dr = new DraggableRectangle(10, 10);
-					dr.setX(corners[0].getX());
-					dr.setY(corners[0].getY());
-					root.getChildren().add(dr);
-					mouseIsPressedRect = true;
-				});
-
-				root.addEventHandler(MouseEvent.MOUSE_DRAGGED, (MouseEvent m) -> {
-					
-					corners[1] = new Point(e.getX(), e.getY());
-					double xdif = corners[0].xdif(corners[1]);
-					double ydif = corners[0].ydif(corners[1]);
-					r.setWidth(xdif);
-					r.setHeight(ydif);
-					// put these lines back in to get tracking in all 4 quadrants
-					r.setX(min(corners[0].getX(), corners[1].getX()));
-					r.setY(min(corners[0].getY(), corners[1].getY()));
-					
-					corners[1] = new Point(m);
-					double xdif = corners[0].xdif(corners[1]);
-					double ydif = corners[0].ydif(corners[1]);
-					dr.setWidth(xdif);
-					dr.setHeight(ydif);
-					// put these lines back in to get tracking in all 4 quadrants
-					dr.setX(min(corners[0].getX(), corners[1].getX()));
-					dr.setY(min(corners[0].getY(), corners[1].getY()));
-				});
-				
-				root.addEventHandler(MouseEvent.MOUSE_RELEASED, (MouseEvent m) -> {
-					
-					System.out.println("Layout X: " + r.getX());
-					System.out.println("Layout Y: " + r.getY());
-					//rectangleList.add(r);
-					 
-					 
-					System.out.println("Layout X: " + dr.getX());
-					System.out.println("Layout Y: " + dr.getY());
-					dr.setimAt(dr.getX(), dr.getY());
-					mouseIsPressedRect = false;
-					drList.add(dr);
-					drawRectangleBool = false;
-				});
-
-			}
-		}
-		*/
-		public double min(double x, double y) {
-			return (x < y) ? x : y;
-		}
-
-		/*
-		public void drawLine() {
-			if (drawLineBool) {
-				System.out.println("Drawing Line.");
-				root.setOnMousePressed(e -> {
-					l = new Line(e.getX(), e.getY(), e.getX(), e.getY());
-					root.getChildren().add(l);
-					// l = new MyLine(e.getX(), e.getY(), e.getX(), e.getY());
-				});
-
-				root.setOnMouseDragged(e -> {
-					l.setEndX(e.getX());
-					l.setEndY(e.getY());
-				});
-			}
-		}
-		 */
-		
-//		public void putText() {
-//			if (putTextBool) {
-//				System.out.println("Putting text on screen.");
-//
-//				String textFieldInput = textField.getText();
-//				System.out.println("Text Field Input: " + textFieldInput);
-//
-//				label = new Label(textFieldInput);
-//				double randX = Math.random() * 800;
-//				double randY = Math.random() * 800;
-//				System.out.println("randx: " + randX + " randy: " + randY);
-//				label.relocate(randX, randY);
-//				// label.setText(textFieldInput);
-//
-//				root.getChildren().add(label);
-//
-//			}
-//		}
-		
-		
-//		public void select() {
-//			root.addEventHandler
-//	        (  MouseEvent.MOUSE_PRESSED,
-//	           (MouseEvent m) ->
-//	           {
-//	               Point p = new Point( m );
-//	               theDR = findDraggedRectangle( p );
-//	               mouseIsPressed = true;
-//	           }
-//	        );
-//			
-//			root.addEventHandler
-//	        (  MouseEvent.MOUSE_RELEASED,
-//	           (MouseEvent m) ->  { mouseIsPressed = false; }
-//	        );
-//			
-//			root.addEventHandler
-//	        (  MouseEvent.MOUSE_DRAGGED,
-//	           (MouseEvent m)->
-//	           {   
-//	               Point p = new Point( m );
-//	               if(theDR!=null) {theDR.dragged(p); }
-//	           }
-//	        );
-//		}
-		
-//		public Rectangle findRectangle(Point p) {
-//			Rectangle found = null;
-//			for (Rectangle r: rectangleList) {
-//				if ()
-//			}
-//		}
-		
-
-	}
-	
-	
-	
-//	public class DraggableShape extends Shape {
-//		
-//	}
-	
-	/*
-	public class DraggableRectangle extends Rectangle {
-		private double mouseX;
-		private double mouseY;
-		Point location;
-		
-		private boolean canDragRectangle = false;
-
-		public DraggableRectangle() {
-			super(10,10);
 			
-			
-
-//			setOnMousePressed(e -> {
-//				mouseX = e.getSceneX();
-//				mouseY = e.getSceneY();
-//			});
-//
-//			setOnMouseDragged(e -> {
-//				double deltaX = e.getSceneX() - mouseX;
-//				double deltaY = e.getSceneY() - mouseY;
-//
-//				relocate(getLayoutX() + deltaX, getLayoutY() + deltaY);
-//				mouseX = e.getSceneX();
-//				mouseY = e.getSceneY();
-//			});
-			
-			
-		}
-		
-		
-		public void drawRectangle() {
-			root.setOnMousePressed(e -> {
-				corners[0] = new Point(e.getX(), e.getY());
-				setX(corners[0].getX());
-				setY(corners[0].getY());
-				//root.getChildren().add(this);
-			});
-			
-			root.setOnMouseDragged(e -> {
-				corners[1] = new Point(e.getX(), e.getY());
-				double xdif = corners[0].xdif(corners[1]);
-				double ydif = corners[0].ydif(corners[1]);
-				setWidth(xdif);
-				setHeight(ydif);
-				// put these lines back in to get tracking in all 4 quadrants
-				setX(min(corners[0].getX(), corners[1].getX()));
-				setY(min(corners[0].getY(), corners[1].getY()));
-			});
-			
-//			root.setOnMouseReleased(e -> {
-//				System.out.println("Layout X: " + getLayoutX());
-//				System.out.println("Layout Y: " + getLayoutY());
-//			});
-			
-			this.canDragRectangle = true;
+			root.addEventHandler
+	        (  MouseEvent.MOUSE_RELEASED,
+	           (MouseEvent m) ->
+	           {
+	               mouseIsPressed = false;
+	           }
+	        );
 		}
 		
 		public double min(double x, double y) {
 			return (x < y) ? x : y;
 		}
+
 		
-//		public boolean zatYou(Point m) {
-//			
-//		}
-	}
-	*/
+		
+		public void putText() {
+			if (putTextBool) {
+				System.out.println("Putting text on screen.");
+
+				String textFieldInput = textField.getText();
+				System.out.println("Text Field Input: " + textFieldInput);
+
+				label = new Label(textFieldInput);
+				double randX = Math.random() * 800;
+				double randY = Math.random() * 800;
+				System.out.println("randx: " + randX + " randy: " + randY);
+				label.relocate(randX, randY);
+				// label.setText(textFieldInput);
+
+				root.getChildren().add(label);
+
+			}
+		}
+		
+		
+
 	
+	}
 }
